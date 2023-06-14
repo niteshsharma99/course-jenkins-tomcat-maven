2ff71eec02f8e159fecea199edd8aee3bb0055d0
Nitesh Sharma <75922520+niteshsharma99@users.noreply.github.com>
2023-06-14 17:15:42 +0530

    Update HelloController.java

--- src/main/java/com/course/tomcat/controller/HelloController.java
+++ src/main/java/com/course/tomcat/controller/HelloController.java
@@ -1,27 +1,28 @@
 package com.course.tomcat.controller;
- 
+
 import org.springframework.web.bind.annotation.*;
- 
+
 import java.time.LocalDateTime;
 import java.util.concurrent.ThreadLocalRandom;
- 
+
 @RestController
 @RequestMapping("/api")
 public class HelloController {
- 
+
     @GetMapping(value = {"", "/", "/now"})
     String hello() {
         return "Now is " + LocalDateTime.now();
     }
-    
+
     @GetMapping(value = "/random")
     int random(){
         return ThreadLocalRandom.current().nextInt(0,1000);
     }
-# add a comment
+
+    // Add a comment
     @GetMapping(value = "/hero")
     String hero(){
         return "My favorite superhero is Batman";
     }
- 
+
 }
